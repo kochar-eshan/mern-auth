429ebb97e643977a5720df7889012503b13780c2
kochar-eshan <eshankochar06@gmail.com>
2025-12-08 15:58:47 +0530

    creating routes for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import SignUp from './pages/SignUp.jsx';
+import SignIn from './pages/Signin.jsx';
+import Profile from './pages/Profile.jsx';
 
 export default function App() {
-  return <div className='text-red-500'>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
