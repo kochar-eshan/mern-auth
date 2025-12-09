a7215cc8a2ca92109c1be05840d4dae319bdd553
kochar-eshan <eshankochar06@gmail.com>
2025-12-09 19:13:23 +0530

    middleware added to handle error

--- api/index.js
+++ api/index.js
@@ -1,27 +1,33 @@
 import express from 'express';
-import mongoose from 'mongoose';  
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB:', err);
-});
-
-
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 const app = express();
-const PORT = process.env.PORT || 5000;  
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 app.use(express.json());
 
-app.use("/api/user",userRoutes);
-app.use("/api/auth",authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({ message, success: false });
+});
