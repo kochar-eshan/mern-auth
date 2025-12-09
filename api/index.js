8356c1b06393bd0f367cbdb389869b90cd082514
kochar-eshan <eshankochar06@gmail.com>
2025-12-09 13:54:41 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';  
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;  
