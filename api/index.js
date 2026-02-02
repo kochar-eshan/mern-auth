ba5a1aa4ccafa723a9c13761bff27771c75ae8c6
kochar-eshan <eshankochar06@gmail.com>
2026-02-02 22:48:55 +0530

    ready to deploy

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -16,7 +17,14 @@ mongoose
     console.error('Error connecting to MongoDB:', err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use('*', (req, res, next) => {
+res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
